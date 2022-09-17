d20329d06509c9da8a21bc642f3d2005a22cf000
felix <dongzhew@usc.edu>
2022-09-17 13:25:02 -0700

    Add initial start page (overview)

---
+++ app/src/main/java/com/example/minesweeper/grid.java
@@ -0,0 +1,13 @@
+package com.example.minesweeper;
+
+public class grid {
+    public boolean mine = false;
+    public boolean empty = true;
+    public boolean revealed = false;
+    public boolean flagged = false;
+
+    public grid(){
+
+    }
+}
+
